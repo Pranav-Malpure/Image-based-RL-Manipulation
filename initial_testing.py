e5ab70db9f6986f9f4cf52af1c755d0c3bc88094
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-25 12:11:35 -0700

    added sac_agent

--- initial_testing.py
+++ initial_testing.py
@@ -23,17 +23,15 @@ import mani_skill.envs
 import torch
 import matplotlib.pyplot as plt
 import time
-
+from sac_image import Args, SAC
 
 env_id = "PickCube-v1"
 obs_mode = "rgbd"
 control_mode = "pd_joint_delta_pos"
 reward_mode = "sparse"
 robot_uids = "panda"
-
-
-
-num_envs = 1 # you can go up to 4096 on better GPUs
+num_envs = 1
+num_eval_envs = 1
 
 
 try:
@@ -48,25 +46,30 @@ if IN_COLAB:
 
 
 def main():
-    env = gym.make(env_id, num_envs=num_envs, obs_mode=obs_mode, control_mode=control_mode, 
-                   reward_mode=reward_mode, robot_uids=robot_uids, enable_shadow=True)
-    env.unwrapped.print_sim_details()
-    obs, info_reset = env.reset()
-    print("Observation Space:", obs)
-    print("Info:", info_reset)
-    print("Action Space:", env.action_space)
-    done = False
-    start_time = time.time()
-    total_rew = 0
-    obs, rew, terminated, truncated, info = env.step(torch.from_numpy(env.action_space.sample()))
-    # while not done:
-    #     # note that env.action_space is now a batched action space
-    #     obs, rew, terminated, truncated, info = env.step(torch.from_numpy(env.action_space.sample()))
-    #     done = (terminated | truncated).any() # stop if any environment terminates/truncates
-    N = num_envs * info["elapsed_steps"][0].item()
-    dt = time.time() - start_time
-    FPS = N / (dt)
-    print(f"Frames Per Second = {N} / {dt} = {FPS}")
+
+
+    args = Args(env_id=env_id, num_envs=num_envs, num_eval_envs=num_eval_envs, obs_mode=obs_mode, control_mode=control_mode, 
+                reward_mode=reward_mode, robot_uids=robot_uids, enable_shadow=True)
+
+    sac_agent = SAC(args)
+    sac_agent.start_game()
+    # env.unwrapped.print_sim_details()
+    # obs, info_reset = env.reset()
+    # print("Observation Space:", obs)
+    # print("Info:", info_reset)
+    # print("Action Space:", env.action_space)
+    # done = False
+    # start_time = time.time()
+    # total_rew = 0
+    # obs, rew, terminated, truncated, info = env.step(torch.from_numpy(env.action_space.sample()))
+    # # while not done:
+    # #     # note that env.action_space is now a batched action space
+    # #     obs, rew, terminated, truncated, info = env.step(torch.from_numpy(env.action_space.sample()))
+    # #     done = (terminated | truncated).any() # stop if any environment terminates/truncates
+    # N = num_envs * info["elapsed_steps"][0].item()
+    # dt = time.time() - start_time
+    # FPS = N / (dt)
+    # print(f"Frames Per Second = {N} / {dt} = {FPS}")
 
 
 if __name__ == "__main__":
