ddad203f99bb6b5f83ba069d0b6b4f601ae7d96b
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-10-25 13:21:56 -0700

    added cnn

--- sac_image.py
+++ sac_image.py
@@ -149,7 +149,8 @@ class ReplayBuffer:
         self.num_envs = num_envs
         self.storage_device = storage_device
         self.sample_device = sample_device
-        self.obs = torch.zeros((buffer_size, num_envs) + env.single_observation_space.shape).to(storage_device)
+        # self.obs = torch.zeros((buffer_size, num_envs) + env.single_observation_space.shape).to(storage_device)
+        self.obs = torch.zeros((buffer_size, num_envs) + 128*12*12).to(storage_device)
         self.next_obs = torch.zeros((buffer_size, num_envs) + env.single_observation_space.shape).to(storage_device)
         self.actions = torch.zeros((buffer_size, num_envs) + env.single_action_space.shape).to(storage_device)
         self.logprobs = torch.zeros((buffer_size, num_envs)).to(storage_device)
