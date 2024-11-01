2d0f39d5bf3778dcf01e9dcf405805e4f2f62470
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-01 12:53:16 -0700

    minor bug update

--- sac_image.py
+++ sac_image.py
@@ -199,64 +199,64 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
-class NatureCNN(nn.Module):
-    def __init__(self, sample_obs):
-        super().__init__()
+# class NatureCNN(nn.Module):
+#     def __init__(self, sample_obs):
+#         super().__init__()
 
-        extractors = {}
+#         extractors = {}
 
-        self.out_features = 0
-        feature_size = 256
-        in_channels=sample_obs["rgb"].shape[-1]
-        image_size=(sample_obs["rgb"].shape[1], sample_obs["rgb"].shape[2])
+#         self.out_features = 0
+#         feature_size = 256
+#         in_channels=sample_obs["rgb"].shape[-1]
+#         image_size=(sample_obs["rgb"].shape[1], sample_obs["rgb"].shape[2])
 
 
-        # here we use a NatureCNN architecture to process images, but any architecture is permissble here
-        cnn = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=32,
-                kernel_size=8,
-                stride=4,
-                padding=0,
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=0
-            ),
-            nn.ReLU(),
-            nn.Conv2d(
-                in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0
-            ),
-            nn.ReLU(),
-            nn.Flatten(),
-        )
+#         # here we use a NatureCNN architecture to process images, but any architecture is permissble here
+#         cnn = nn.Sequential(
+#             nn.Conv2d(
+#                 in_channels=in_channels,
+#                 out_channels=32,
+#                 kernel_size=8,
+#                 stride=4,
+#                 padding=0,
+#             ),
+#             nn.ReLU(),
+#             nn.Conv2d(
+#                 in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=0
+#             ),
+#             nn.ReLU(),
+#             nn.Conv2d(
+#                 in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0
+#             ),
+#             nn.ReLU(),
+#             nn.Flatten(),
+#         )
 
-        # to easily figure out the dimensions after flattening, we pass a test tensor
-        with torch.no_grad():
-            n_flatten = cnn(sample_obs["rgb"].float().permute(0,3,1,2).cpu()).shape[1]
-            fc = nn.Sequential(nn.Linear(n_flatten, feature_size), nn.ReLU())
-        extractors["rgb"] = nn.Sequential(cnn, fc)
-        self.out_features += feature_size
+#         # to easily figure out the dimensions after flattening, we pass a test tensor
+#         with torch.no_grad():
+#             n_flatten = cnn(sample_obs["rgb"].float().permute(0,3,1,2).cpu()).shape[1]
+#             fc = nn.Sequential(nn.Linear(n_flatten, feature_size), nn.ReLU())
+#         extractors["rgb"] = nn.Sequential(cnn, fc)
+#         self.out_features += feature_size
 
-        if "state" in sample_obs:
-            # for state data we simply pass it through a single linear layer
-            state_size = sample_obs["state"].shape[-1]
-            extractors["state"] = nn.Linear(state_size, 256)
-            self.out_features += 256
+#         if "state" in sample_obs:
+#             # for state data we simply pass it through a single linear layer
+#             state_size = sample_obs["state"].shape[-1]
+#             extractors["state"] = nn.Linear(state_size, 256)
+#             self.out_features += 256
 
-        self.extractors = nn.ModuleDict(extractors)
+#         self.extractors = nn.ModuleDict(extractors)
 
-    def forward(self, observations) -> torch.Tensor:
-        encoded_tensor_list = []
-        # self.extractors contain nn.Modules that do all the processing.
-        for key, extractor in self.extractors.items():
-            obs = observations[key]
-            if key == "rgb":
-                obs = obs.float().permute(0,3,1,2)
-                obs = obs / 255
-            encoded_tensor_list.append(extractor(obs))
-        return torch.cat(encoded_tensor_list, dim=1)
+#     def forward(self, observations) -> torch.Tensor:
+#         encoded_tensor_list = []
+#         # self.extractors contain nn.Modules that do all the processing.
+#         for key, extractor in self.extractors.items():
+#             obs = observations[key]
+#             if key == "rgb":
+#                 obs = obs.float().permute(0,3,1,2)
+#                 obs = obs / 255
+#             encoded_tensor_list.append(extractor(obs))
+#         return torch.cat(encoded_tensor_list, dim=1)
 
 
 class SoftQNetwork(nn.Module):
@@ -322,7 +322,7 @@ class SoftQNetwork(nn.Module):
                                 nn.ReLU(),
                                 nn.Linear(256, 1))
         extractors["rgb"] = nn.Sequential(cnn, self.fc)
-        self.out_features += feature_size
+        # self.out_features += feature_size
 
         self.extractors = nn.ModuleDict(extractors)
 
