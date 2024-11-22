d73c1f0864425973e51db2e09c6a5e3ed0fc2f0c
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-11-22 01:28:22 -0800

    testing for RL training

--- initial_testing.py
+++ initial_testing.py
@@ -27,7 +27,7 @@ from sac_image import Args, SAC
 # from sac_joints import Args, SAC
 
 env_id = "PickCube-v1"
-obs_mode = "rgbd"
+obs_mode = "rgb+depth"
 control_mode = "pd_joint_delta_pos"
 reward_mode = "sparse"
 robot_uids = "panda"
