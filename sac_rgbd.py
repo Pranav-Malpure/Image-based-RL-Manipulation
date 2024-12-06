7d379c4f9c87377ba96b62e001a7b9eaf198d65d
Pranav-Malpure <pranavmalpure2020@gmail.com>
2024-12-06 02:14:52 -0800

    debugging

--- sac_rgbd.py
+++ sac_rgbd.py
@@ -386,11 +386,13 @@ class SoftQNetwork(nn.Module):
         action_dim = np.prod(envs.single_action_space.shape)
         state_dim = envs.single_observation_space['state'].shape[0]
 
-        self.trunk = nn.Sequential(nn.Linear(self.encoder.encoder.out_dim*action_dim +state_dim, 512), nn.LayerNorm(512), nn.Tanh()) # TODO: have to check this 512
+        self.trunk = nn.Sequential(nn.Linear(self.encoder.encoder.out_dim*action_dim + state_dim, 512), nn.LayerNorm(512), nn.Tanh()) # TODO: have to check this 512
         self.mlp = make_mlp(512, [512, 256, 1], last_act=False)  #TODO check this for 512 input
         # self.mlp = make_mlp(encoder.encoder.out_dim+action_dim+state_dim, [512, 256, 1], last_act=False)
 
     def forward(self, obs, action, visual_feature=None, detach_encoder=False):
+        if obs is None:
+            print("here is the mistake")
         if visual_feature is None:
             visual_feature = self.encoder(obs)
         if detach_encoder:
